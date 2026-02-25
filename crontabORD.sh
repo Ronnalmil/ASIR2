40e120331e1b1c5624e3cf1685a53393303406f2
ronnytron <ronnal2020@gmail.com>
2026-02-25 07:50:02 +0100

    Actualizar crontabORD.sh

--- crontabORD.sh
+++ crontabORD.sh
@@ -5,3 +5,11 @@ ls -la ruta,
 date 
 
 * * * * * /usr/bin/date  >  /tmp/time.txt
+
+# Casos :  Todos los dias a las 3AM
+m H d M s
+* 3 * * * /usr/bin/date  > /tmp/time.txt
+
+# Caso : Todos los 1 de cada enero y Lunes
+* 3 1 1 1
+
